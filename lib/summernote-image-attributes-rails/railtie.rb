00b13b81b9400781cb3dc6b2e187e02ced4faeaa
Adrian Pradilla <adrianpradilla@gmail.com>
2017-05-17 11:04:28 +0200

    Add lib files

---
+++ lib/summernote-image-attributes-rails/railtie.rb
@@ -0,0 +1,5 @@
+module SummernoteImageAttributesRails
+  module Rails
+    class Railtie < ::Rails::Railtie; end
+  end
+end
---
+++ lib/summernote-image-attributes-rails/version.rb
@@ -0,0 +1,5 @@
+module SummernoteImageAttributesRails
+  module Rails
+    VERSION = '0.8.3'
+  end
+end
